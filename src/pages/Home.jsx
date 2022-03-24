824c0d49455044f3a82105a66a874a63fd599275
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-03-24 15:42:10 -0300

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductsList from "../components/ProductsList";
 import Header from "../components/Header";
 
 function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect", process.env.REACT_APP_API_URL);
+    const getProducts = async () => {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/products`);
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    getProducts();
+  }, []);
+
   return (
     <div className="home">
       <Header />
       <h2 className="fs-4 text-center mb-5 fw-bold"> DESTACADOS</h2>
-      <ProductsList />
+      <ProductsList products={products} />
     </div>
   );
 }
