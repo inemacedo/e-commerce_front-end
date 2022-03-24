3d36edb2d966a390f71d98e7480009d1fcdd9d24
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-03-24 16:14:04 -0300

    async en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,15 @@ function Home() {
 
   useEffect(() => {
     console.log("useEffect", process.env.REACT_APP_API_URL);
-    const getProducts = ()=>{
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/products?featured=true`);
+    const getProducts = async () => {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/products?featured=true`
+      );
       const data = await response.json();
       console.log(data);
       setProducts(data);
     };
     getProducts();
-
   }, []);
 
   return (
