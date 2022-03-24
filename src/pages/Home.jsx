1cd29df435a5e4e00ca379ec14aff46c9bc75a84
Andy Nievas <nievasandy73@gmail.com>
2022-03-24 14:58:24 -0300

    .env.example y fetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductsList from "../components/ProductsList";
 import Header from "../components/Header";
 
 function Home() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect( ()=>{
+    console.log("useEffect", process.env.REACT_APP_API_URL);
+    const getProducts = async ()=>{
+      const response = await fetch(`${"http://localhost:8000"}/products`);
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    }
+    getProducts();
+  } , [] )
+
   return (
     <div className="home">
       <Header />
-      <ProductsList />
+      <ProductsList products={products} />
     </div>
   );
 }
