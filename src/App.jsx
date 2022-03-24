8e934067f4b17a557dbebcd2e91ab326627add58
Valentina Giusti <valentinamgiusti@gmail.com>
2022-03-23 22:18:52 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
 
-
 function App() {
   return (
     <div className="App">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ProductsList from "../components/ProductsList";
+import NavbarComponent from "../components/NavbarComponent";
 
 function Home() {
-  return <div className="home" >
-    {/* <Header /> */}
-    <ProductsList />
-  </div>;
+  return (
+    <div className="home">
+      <NavbarComponent />
+      {/* <Header /> */}
+      <ProductsList />
+    </div>
+  );
 }
 
 export default Home;
