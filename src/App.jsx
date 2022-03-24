8daff274836de8cdb376e8db4562edb73d2b273b
Valentina Giusti <valentinamgiusti@gmail.com>
2022-03-23 22:20:31 -0300

    Navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
+import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <NavbarComponent />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ProductsList from "../components/ProductsList";
-import NavbarComponent from "../components/NavbarComponent";
 
 function Home() {
   return (
     <div className="home">
-      <NavbarComponent />
       {/* <Header /> */}
       <ProductsList />
     </div>
