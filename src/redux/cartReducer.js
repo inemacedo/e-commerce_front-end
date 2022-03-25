9f9bd56a9405c40656b206ce49e408ba259188ea
Andy Nievas <nievasandy73@gmail.com>
2022-03-25 14:34:26 -0300

    ADD_ITEM_CART reducer

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -3,17 +3,20 @@ export function cartReducer(cart = [], action) {
   switch (action.type) {
     case "ADD_ITEM_CART":{
       // Si el id es distinto, agrego el elem nuevo, de lo contrario sumo una unidad
-      // for (const item of cart) {
-      //   if(item.id === action.payload.id){
-      //     // Sumo una unidad
-      //     item.units += 1;
-      //   }
-      //   else{
-      //     // Agrego el item nuevo
-      //   }
-      // }
-      return [...cart, ...action.payload];
-      
+      const newCart = [];
+      let found = false;
+      for (let i=0; i<cart.length && !found ; i++) {
+        if(cart[i].id === action.payload.id){
+          cart[i].units += 1;
+          found = true;
+        }
+        newCart.push(cart[i]);
+      }
+      if(!found){
+        action.payload.units = 1;
+        newCart.push(action.payload);
+      }
+      return newCart;
     }
     case "REMOVE_ITEM_CART":
       return cart;
