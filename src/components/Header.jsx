91764230246a4b0a63d76c0a63c09a44f450ee54
inemacedo <93352798+inemacedo@users.noreply.github.com>
2022-04-13 10:15:58 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,11 @@ import { Link } from "react-router-dom";
 function Header({ title }) {
   return (
     <div className="home-images-responsive header-background d-flex flex-column align-items-center justify-content-center">
-      <h1 className="text-light m-5 m-md-0">
-        {title ? title : "Tu casa, tu lugar en el mundo"}
-      </h1>
+      <div>
+        <h1 className="text-light m-5 m-md-0">
+          {title ? title : "Tu casa, tu lugar en el mundo"}
+        </h1>
+      </div>
       <Link className="btn btn-lg btn-light mt-5" to="/productos">
         Explorar
       </Link>
